ede3670ba78ad9a535a81baaf86e38c4afa3355a
Danielle Heberling <deeheber@users.noreply.github.com>
2024-05-05 17:00:31 -0700

    Initial commit

---
+++ test/aws-serverless-url-shortener.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessUrlShortener from '../lib/aws-serverless-url-shortener-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-url-shortener-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessUrlShortener.AwsServerlessUrlShortenerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
